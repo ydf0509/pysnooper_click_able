bebb5c1b4be85ef658f18a1d9faaef866780ce8c
k590s <909686719@qq.com>
2020-06-19 08:09:03 +0800

    no message

---
+++ tests/test_pysnooper2.py
@@ -0,0 +1,35 @@
+# coding=utf8
+"""
+@author:Administrator
+@file: test_pysnooper2.py
+@time: 2020/06
+"""
+
+from pysnooper_click_able import snoop
+
+
+def f1(x):
+    if x == 1:
+        a = 2
+    else:
+        a = 3
+
+
+def f2(x):
+    if x == 7:
+        b = 8
+        for i in range(1000):
+            b += i
+    else:
+        b = 9
+
+
+@snoop(depth=9)
+def f3(x, test=True):
+    if test:
+        f1(x)
+    else:
+        f2(x)
+
+
+f3(5, False)
